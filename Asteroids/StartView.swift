a18063c41231d57bd312aa876b8149e39b1d5483
Jonathan French <jaypeeff@gmail.com>
2024-08-07 23:40:16 +0300

    Launch screen and changed the look of the game. Plus a few minor tweeks.

---
+++ Asteroids/StartView.swift
@@ -0,0 +1,18 @@
+//
+//  StartView.swift
+//  Asteroids
+//
+//  Created by Jonathan French on 7.08.24.
+//
+
+import SwiftUI
+
+struct StartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartView()
+}
